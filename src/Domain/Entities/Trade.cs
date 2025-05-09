8a1a0d65dadd5ed2beca6155441953949b095f4f
GabrielRego1 <gabriel.antonio280@gmail.com>
2025-05-08 22:24:42 -0300

    feat: start trade model domain

--- src/Domain/Entities/Trade.cs
+++ src/Domain/Entities/Trade.cs
@@ -1,6 +1,13 @@
-﻿namespace Domain.Entities;
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
 
 public class Trade
 {
     public string Ticker { get; set; }
+    public string Account { get; set; }
+    public Side Side { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly TradeDate { get; set; }
 }
---
+++ src/Domain/Enums/Side.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Enums;
+
+public enum Side
+{
+    Buy,
+    Sell
+}
