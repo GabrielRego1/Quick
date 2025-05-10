df7c4a47fb434f1c72f57ace8276fe7566dc0336
GabrielRego1 <gabriel.antonio280@gmail.com>
2025-05-09 21:28:43 -0300

    feat: modeling trade entity

--- src/Domain/Entities/Trade.cs
+++ src/Domain/Entities/Trade.cs
@@ -4,10 +4,31 @@ namespace Domain.Entities;
 
 public class Trade
 {
-    public string Ticker { get; set; }
-    public string Account { get; set; }
-    public Side Side { get; set; }
-    public decimal Quantity { get; set; }
-    public decimal Price { get; set; }
-    public DateOnly TradeDate { get; set; }
+    public string Ticker { get; private set; }
+    public string Account { get; private set; }
+    public string SettlmentAccount { get; private set; }
+    public Side Side { get; private set; }
+    public decimal Quantity { get; private set; }
+    public decimal Price { get; private set; }
+    public DateOnly TradeDate { get; private set; }
+
+    public Trade(string ticker, string account, string settlmentAccount, Side side, decimal quantity, decimal price,
+        DateOnly tradeDate)
+    {
+        Ticker = ticker;
+        Account = account;
+        SettlmentAccount = settlmentAccount;
+        Side = side;
+        Quantity = quantity;
+        Price = price;
+        TradeDate = tradeDate;
+    }
+
+    public void UpdateSettlmentAccount(string settlmentAccount)
+    {
+        if (string.IsNullOrEmpty(settlmentAccount))
+            throw new ArgumentException("Invalid settlement account");
+
+        SettlmentAccount = settlmentAccount;
+    }
 }
