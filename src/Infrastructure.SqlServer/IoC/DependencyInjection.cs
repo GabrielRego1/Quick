b3293db358dca11821ce164dc9250d820e6203b9
GabrielRego1 <gabriel.antonio280@gmail.com>
2025-05-12 21:23:40 -0300

    feat: add SQL Server dependency injection configuration

---
+++ src/Infrastructure.SqlServer/IoC/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Application.Abstractions.Repositories;
+using Infrastructure.SqlServer.Contexts;
+using Infrastructure.SqlServer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.SqlServer.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSqlServer(this IServiceCollection services, IConfiguration configuration)
+        => services.AddDbContext<QuickDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("QuickConnection")))
+            .AddRepositories();
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+        => services.AddScoped(typeof(IRepository<>), typeof(Repository<>))
+            .AddScoped<ITradeRepository, TradeRepository>();
+}
