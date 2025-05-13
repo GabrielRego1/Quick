cfe938a50a6695c70e11eb5c4263b0dee6c8ecc6
GabrielRego1 <gabriel.antonio280@gmail.com>
2025-05-12 21:23:48 -0300

    feat: add QuickDbContext with trade configuration

---
+++ src/Infrastructure.SqlServer/Contexts/QuickDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.SqlServer.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.SqlServer.Contexts;
+
+internal class QuickDbContext(DbContextOptions<QuickDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new TradeConfiguration());
+    }
+}
