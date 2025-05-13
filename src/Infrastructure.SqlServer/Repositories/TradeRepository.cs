cf857709605bce35edb89dc1e730660632f95ced
GabrielRego1 <gabriel.antonio280@gmail.com>
2025-05-12 21:23:33 -0300

    feat: implement trade repository and interface

---
+++ src/Infrastructure.SqlServer/Repositories/TradeRepository.cs
@@ -0,0 +1,7 @@
+﻿using Application.Abstractions.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.SqlServer.Repositories;
+
+internal class TradeRepository(DbContext context) : Repository<Trade>(context), ITradeRepository;
