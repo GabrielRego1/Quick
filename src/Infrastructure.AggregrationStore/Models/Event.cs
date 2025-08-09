c8dbf3c94335a5d44e78b3a91639dc8125fb55b7
GabrielRego1 <gabriel.antonio280@gmail.com>
2025-08-08 21:26:31 -0300

    refactor: update Entity Framework Core packages and refactor Event record for improved readability

--- src/Infrastructure.AggregrationStore/Models/Event.cs
+++ src/Infrastructure.AggregrationStore/Models/Event.cs
@@ -1,21 +1,37 @@
-﻿using Domain.Abstractions;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Abstractions;
 using Domain.Aggregates;
 
 namespace Infrastructure.AggregrationStore.Models;
 
-public record Event(
-    long Id,
-    int Version,
-    string AggregateId,
-    string AggregateName,
-    string EventName,
-    IEvent Data,
-    DateTimeOffset Timestamp)
+public record Event
 {
+    public long Id { get; private set; }
+    public int Version { get; private set; }
+    public string AggregateId { get; private set; }
+    public string AggregateName { get; private set; }
+    public string EventName { get; private set; }
+    public IEvent Data { get; private set; }
+    public DateTimeOffset Timestamp { get; private set; }
+
+    private Event()
+    {
+    }
+
     public const int AggregateIdMaxLength = 100;
     public const int AggregateNameMaxLength = 100;
     public const int MessageNameMaxLength = 100;
 
-    public static Event Create(int version, IAggregateId id, IAggregateRoot aggregate, IEvent @event,DateTimeOffset timestamp) 
-        => new(0, version, id.ParseToString(), aggregate.GetType().Name, @event.GetType().Name, @event, timestamp);
+    public static Event Create(int version, IAggregateId id, IAggregateRoot aggregate, IEvent @event,
+        DateTimeOffset timestamp)
+        => new()
+        {
+            Id = 0,
+            Version = version,
+            AggregateId = id.ParseToString(),
+            AggregateName = aggregate.GetType().Name,
+            EventName = @event.GetType().Name,
+            Data = @event,
+            Timestamp = timestamp,
+        };
 }
